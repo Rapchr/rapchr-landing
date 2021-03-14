39cb36a34ade47f884cbcabf502d944b34c7a5a6
Malcolm Bouzi <malcolmbouzi@Malcolms-Air.home>
2021-03-14 17:09:52 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import MailchimpSubscribe from "react-mailchimp-subscribe"
+
 
 function App() {
+  const url = process.env.REACT_APP_MAILCHIMP_URL;
+
+  const SimpleForm = () => <MailchimpSubscribe url={url}/>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="landing-page">
+      <div className="slanding-page-ontents">
+        <img className="landing-page-logo" src={logo} alt="Rapchr Logo" />
+        <div className="landing-page-contents">
+          <p className="landing-page-msg">Hip-hop tech platform focused on community, culture, and the art of rap. Sign up to find out more about what we're building, and follow us on twitter <a target="_blank" className="splash-page__msg__link" href="https://twitter.com/rapchr">@rapchr</a></p>
+        </div>
+        <MailchimpSubscribe
+          url={url}
+          render={({ subscribe, status, message }) => (
+            <div>
+              <SimpleForm url={url} onSubmitted={formData => subscribe(formData)} />
+              {status === "sending" && <div style={{ color: "orange", marginBottom: "20px" }}>sending...</div>}
+              {status === "error" && <div style={{ color: "red" + 10, marginBottom: "20px" }} dangerouslySetInnerHTML={{__html: message}}/>}
+              {status === "success" && <div style={{ color: "orange", marginBottom: "20px" }}>Subscribed !</div>}
+            </div>
+          )}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
