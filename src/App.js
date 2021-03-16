9990bd19087284a56a92b75ab31259e460f803dd
Malcolm Bouzi <malcolmbouzi@Malcolms-Air.home>
2021-03-15 23:58:28 -0400

    file set up

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 import MailchimpSubscribe from "react-mailchimp-subscribe"
+import logo from "./logo.svg"
 
 
-function App() {
-  const url = process.env.REACT_APP_MAILCHIMP_URL;
+class App extends Component {
 
-  const SimpleForm = () => <MailchimpSubscribe url={url}/>
+  constructor() {
+    super();
 
-  return (
-    <div className="landing-page">
-      <div className="slanding-page-ontents">
-        <img className="landing-page-logo" src={logo} alt="Rapchr Logo" />
+  }
+
+
+  render() {
+    const url = process.env.REACT_APP_MAILCHIMP_URL;
+
+    const SimpleForm = () => <MailchimpSubscribe url={url}/>
+
+    return (
+      <div className="landing-page">
         <div className="landing-page-contents">
-          <p className="landing-page-msg">Hip-hop tech platform focused on community, culture, and the art of rap. Sign up to find out more about what we're building, and follow us on twitter <a target="_blank" className="splash-page__msg__link" href="https://twitter.com/rapchr">@rapchr</a></p>
+          <img className="landing-page-logo" src={logo} alt="Rapchr Logo" />
+          <div className="landing-page-contents">
+            <p className="landing-page-msg">A social platform for rap fans. <a target="_blank" rel="noreferrer" className="splash-page__msg__link" href="https://twitter.com/rapchr">@rapchr</a></p>
+          </div>
+          <MailchimpSubscribe
+            url={url}
+            render={({ subscribe, status, message }) => {
+              return (
+                <div>
+                  <SimpleForm url={url} onSubmitted={formData => subscribe(formData)} />
+                  {status === "sending" && <div style={{ color: "#c77d4a", marginBottom: "20px" }}>sending...</div>}
+                  {status === "error" && <div style={{ color: "#722a1f" + 10, marginBottom: "20px" }} dangerouslySetInnerHTML={{__html: message}}/>}
+                  {status === "success" && <div style={{ color: "#c77d4a", marginBottom: "20px" }}>Subscribed !</div>}
+                </div>
+              )
+            }}
+          />
         </div>
-        <MailchimpSubscribe
-          url={url}
-          render={({ subscribe, status, message }) => (
-            <div>
-              <SimpleForm url={url} onSubmitted={formData => subscribe(formData)} />
-              {status === "sending" && <div style={{ color: "orange", marginBottom: "20px" }}>sending...</div>}
-              {status === "error" && <div style={{ color: "red" + 10, marginBottom: "20px" }} dangerouslySetInnerHTML={{__html: message}}/>}
-              {status === "success" && <div style={{ color: "orange", marginBottom: "20px" }}>Subscribed !</div>}
-            </div>
-          )}
-        />
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App;
